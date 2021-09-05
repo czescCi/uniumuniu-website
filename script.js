043ee1803bacf42013b9ed7ed2f3e84330c86a4f
AsiaG <asia.godula@gmail.com>
2021-09-05 18:45:43 +0200

    Updating page for mobile

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-if (screen.width <= 800) {
-    document.location = "mobilepage.html";
+// if (screen.width <= 800) {
+//     document.location = "mobilepage.html";
+// }
+
+function activateNavigation() {
+    const sections = document.querySelectorAll(".section");
+    const navContainer = document.createElement("nav");
+
 }
+
+activateNavigation();
