9e5c1be5fe4e69d399b03031d2fbfb358794283d
AsiaG <asia.godula@gmail.com>
2021-09-07 09:10:38 +0200

    Adding text area

--- script.js
+++ script.js
@@ -5,7 +5,39 @@
 function activateNavigation() {
     const sections = document.querySelectorAll(".section");
     const navContainer = document.createElement("nav");
-
-}
-
-activateNavigation();
+    const navItems = Array.from(sections).map((section) => {
+      return `
+                      <div class="nav-item" data-for-section="${section.id}">
+                          <a href="#${section.id}" class="nav-link"></a>
+                          <span class="nav-label">${section.dataset.label}</span>
+                      </div>
+                  `;
+    });
+  
+    navContainer.classList.add("nav");
+    navContainer.innerHTML = navItems.join("");
+  
+    const observer = new IntersectionObserver(
+      (entries) => {
+        document.querySelectorAll(".nav-link").forEach((navLink) => {
+          navLink.classList.remove("nav-link-selected");
+        });
+  
+        const visibleSection = entries.filter((entry) => entry.isIntersecting)[0];
+  
+        document
+          .querySelector(
+            `.nav-item[data-for-section="${visibleSection.target.id}"] .nav-link`
+          )
+          .classList.add("nav-link-selected");
+      },
+      { threshold: 0.5 }
+    );
+  
+    sections.forEach((section) => observer.observe(section));
+  
+    document.body.appendChild(navContainer);
+  }
+  
+  activateNavigation();
+  
