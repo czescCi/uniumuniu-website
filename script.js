c2583c6146bc7ae287515fa62160b5789442a612
Asia <asia.godula@gmail.com>
2021-09-22 20:45:12 +0200

    Desktop website, adjusting nav divs

--- script.js
+++ script.js
@@ -2,58 +2,72 @@
 //     document.location = "mobilepage.html";
 // }
 
-function activateNavigation() {
-    const sections = document.querySelectorAll(".section");
-    const navContainer = document.createElement("nav");
-    const navItems = Array.from(sections).map((section) => {
-      return `
-                      <div class="nav-item" data-for-section="${section.id}">
-                          <a href="#${section.id}" class="nav-link"></a>
-                          <span class="nav-label">${section.dataset.label}</span>
-                      </div>
-                  `;
-    });
+// function activateNavigation() {
+//     const sections = document.querySelectorAll(".section");
+//     const navContainer = document.createElement("nav");
+//     const navItems = Array.from(sections).map((section) => {
+//       return `
+//                       <div class="nav-item" data-for-section="${section.id}">
+//                           <a href="#${section.id}" class="nav-link"></a>
+//                           <span class="nav-label">${section.dataset.label}</span>
+//                       </div>
+//                   `;
+//     });
   
-    navContainer.classList.add("nav");
-    navContainer.innerHTML = navItems.join("");
+//     navContainer.classList.add("nav");
+//     navContainer.innerHTML = navItems.join("");
   
-    const observer = new IntersectionObserver(
-      (entries) => {
-        document.querySelectorAll(".nav-link").forEach((navLink) => {
-          navLink.classList.remove("nav-link-selected");
-        });
+//     const observer = new IntersectionObserver(
+//       (entries) => {
+//         document.querySelectorAll(".nav-link").forEach((navLink) => {
+//           navLink.classList.remove("nav-link-selected");
+//         });
   
-        const visibleSection = entries.filter((entry) => entry.isIntersecting)[0];
+//         const visibleSection = entries.filter((entry) => entry.isIntersecting)[0];
   
-        document
-          .querySelector(
-            `.nav-item[data-for-section="${visibleSection.target.id}"] .nav-link`
-          )
-          .classList.add("nav-link-selected");
-      },
-      { threshold: 0.5 }
-    );
+//         document
+//           .querySelector(
+//             `.nav-item[data-for-section="${visibleSection.target.id}"] .nav-link`
+//           )
+//           .classList.add("nav-link-selected");
+//       },
+//       { threshold: 0.5 }
+//     );
   
-    sections.forEach((section) => observer.observe(section));
+//     sections.forEach((section) => observer.observe(section));
   
-    document.body.appendChild(navContainer);
-  }
+//     document.body.appendChild(navContainer);
+//   }
   
-  activateNavigation();
+//   activateNavigation();
   
 
 // MY SOLUTION
 
-const logo = document.getElementById('logo');
+// const logo = document.getElementById('logo');
 
-logo.onclick = function() {
-  let splitedFullFileName = this.src.split('/');
-  let fileName = splitedFullFileName[splitedFullFileName.length - 1];
-  if (fileName === "logo.png") {
-    logo.src = "res/logo_orange.png";
-  } else {
-    logo.src = "res/logo.png";
-  }
-}
+// logo.onclick = function() {
+//   let splitedFullFileName = this.src.split('/');
+//   let fileName = splitedFullFileName[splitedFullFileName.length - 1];
+//   if (fileName === "logo.png") {
+//     logo.src = "res/logo_orange.png";
+//   } else {
+//     logo.src = "res/logo.png";
+//   }
+// }
 
 // ///C:/Projects/uniumuniu-website/res/logo.png
+
+let navItem = document.querySelector(".nav-div");
+let navPara = document.querySelector(".nav-para");
+
+let hoverChangingBgdColor = function() {
+  navItem.style.backgroundColor = "#f8a23a";
+  navPara.style.color = "#131b26";
+}
+
+navItem.addEventListener("mouseover", hoverChangingBgdColor())
+
+// navItem.onclick = function(navItem) {
+  
+// }
