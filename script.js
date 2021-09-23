23a7dc7e68257075ddfc7cb365127a64863d2a5b
Asia <asia.godula@gmail.com>
2021-09-23 08:50:06 +0200

    Working on divs functions

--- script.js
+++ script.js
@@ -1,73 +1,22 @@
-// if (screen.width <= 800) {
-//     document.location = "mobilepage.html";
-// }
-
-// function activateNavigation() {
-//     const sections = document.querySelectorAll(".section");
-//     const navContainer = document.createElement("nav");
-//     const navItems = Array.from(sections).map((section) => {
-//       return `
-//                       <div class="nav-item" data-for-section="${section.id}">
-//                           <a href="#${section.id}" class="nav-link"></a>
-//                           <span class="nav-label">${section.dataset.label}</span>
-//                       </div>
-//                   `;
-//     });
-  
-//     navContainer.classList.add("nav");
-//     navContainer.innerHTML = navItems.join("");
-  
-//     const observer = new IntersectionObserver(
-//       (entries) => {
-//         document.querySelectorAll(".nav-link").forEach((navLink) => {
-//           navLink.classList.remove("nav-link-selected");
-//         });
-  
-//         const visibleSection = entries.filter((entry) => entry.isIntersecting)[0];
-  
-//         document
-//           .querySelector(
-//             `.nav-item[data-for-section="${visibleSection.target.id}"] .nav-link`
-//           )
-//           .classList.add("nav-link-selected");
-//       },
-//       { threshold: 0.5 }
-//     );
-  
-//     sections.forEach((section) => observer.observe(section));
-  
-//     document.body.appendChild(navContainer);
-//   }
-  
-//   activateNavigation();
-  
-
-// MY SOLUTION
-
-// const logo = document.getElementById('logo');
-
-// logo.onclick = function() {
-//   let splitedFullFileName = this.src.split('/');
-//   let fileName = splitedFullFileName[splitedFullFileName.length - 1];
-//   if (fileName === "logo.png") {
-//     logo.src = "res/logo_orange.png";
-//   } else {
-//     logo.src = "res/logo.png";
-//   }
-// }
-
-// ///C:/Projects/uniumuniu-website/res/logo.png
-
 let navItem = document.querySelector(".nav-div");
 let navPara = document.querySelector(".nav-para");
 
-let hoverChangingBgdColor = function() {
-  navItem.style.backgroundColor = "#f8a23a";
-  navPara.style.color = "#131b26";
+let mouseOverBgdColor = function() {
+    navItem.style.backgroundColor === "#f8a23a";
+    navPara.style.color === "#131b26";
 }
 
-navItem.addEventListener("mouseover", hoverChangingBgdColor())
+let mouseOutBgdColor = function() {
+  navItem.style.backgroundColor = "#131b26";
+  navPara.style.color = "#f8a23a";
+}
 
-// navItem.onclick = function(navItem) {
-  
-// }
+navItem.addEventListener("mouseover", mouseOverBgdColor());
+
+navItem.addEventListener("mouseout", mouseOutBgdColor());
+
+
+navItem.onclick = function() {
+  navItem.style.flexGrow = "6";
+  navPara.style.visibility = "hidden";
+}
