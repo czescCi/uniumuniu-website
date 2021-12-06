af24035489ec24314ca07dc6d53d039fc21cc4b2
Asia <Asia@miniprogramista.home>
2021-12-06 15:13:45 +0100

    Refactoring

--- script.js
+++ script.js
@@ -13,54 +13,30 @@ let contactPara = document.getElementById("contact-para");
 let contactForm = document.getElementById("contact-form");
 let logo = document.getElementById("logo");
 let submitBtn = document.getElementById("submit-btn");
-let smallDivFontSize = "30px";
-let normalDivFontSize = "50px";
 let logoSmallSize = "100px";
 let logoNormalSize = "300px";
-let marginBottom = "30px";
 let bgColorOver = "#f8a23a";
 let bgColor = "#131b26";
-let writingMode = "vertical-rl";
 
-
-// let mouseOverBgdColor = function() {
-//     navItems.style.backgroundColor = "#f8a23a";
-//     navPara.style.color = "#131b26";
-// }
-
-// let mouseOutBgdColor = function() {
-//   navItems.style.backgroundColor = "#131b26";
-//   navPara.style.color = "#f8a23a";
-// }
-
-// navItems.addEventListener("mouseover", mouseOverBgdColor);
-
-// navItems.addEventListener("mouseout", mouseOutBgdColor);
-
-
-// navItems.onclick = function() {
-//   navItems.style.flexGrow = "6";
-//   navPara.style.visibility = "hidden";
-// }
 let changeColor = (div, para, toActive) => {
     if (toActive) {
-        div.style.backgroundColor = bgColorOver;
-        para.style.color = bgColor;
+        // div.style.backgroundColor = bgColorOver;
+        // para.style.color = bgColor;
         submitBtn.style.backgroundColor = bgColor;
         submitBtn.style.color = bgColorOver;
     } else {
-        div.style.backgroundColor = bgColor;
-        para.style.color = bgColorOver;
+        // div.style.backgroundColor = bgColor;
+        // para.style.color = bgColorOver;
         submitBtn.style.backgroundColor = bgColorOver;
         submitBtn.style.color = bgColor;
     }
 }
 
-let changeLogo = (logoSmall) => {
-    if (logoSmall) {
-        logo.style.width = logo.style.height = logoSmallSize;
-    } else {
+let changeLogo = (div) => {
+    if (div.classList.contains("active") && div === aboutDiv) {
         logo.style.width = logo.style.height = logoNormalSize;
+    } else {
+        logo.style.width = logo.style.height = logoSmallSize;
     }
 }
 
@@ -74,66 +50,52 @@ for (let i = 0; i < navItems.length; i++) {
     });
 }
 
-let addActiveClass = (div) => {
+let addActiveClass = (div, para) => {
     div.classList.add("active");
     div.classList.remove("nonactive");
+    para.classList.remove("nonactive-para");
+    para.classList.add("active-para");
 }
 
-let removeActiveClass = (div) => {
+let removeActiveClass = (div, para) => {
     div.classList.remove("active");
     div.classList.add("nonactive");
+    para.classList.remove("active-para");
+    para.classList.add("nonactive-para");
 }
 
-let activeFormatting = (div, nonActiveDiv1, nonActivePara1, nonActivePara2) => {
-    if (div.classList.contains("active") && div === aboutDiv || div === projectsDiv) {
-        // div.style.flexGrow = "6";
-        // div.style.cursor = "default";
-        // nonActiveDiv1.style.alignItems = "stretch";
-        nonActivePara1.style.fontSize = nonActivePara2.style.fontSize = smallDivFontSize;
-        nonActivePara1.style.writingMode = nonActivePara2.style.writingMode = writingMode;
-        nonActivePara1.style.marginBottom = nonActivePara2.style.marginBottom = marginBottom;
-    } else if (div.classList.contains("active") && div === contactDiv) {
-        // div.style.flexGrow = "6";
-        // div.style.cursor = "default";
-        // nonActiveDiv1.style.alignItems = "stretch";
-        nonActivePara1.style.fontSize = nonActivePara2.style.fontSize = smallDivFontSize;
-        nonActivePara1.style.writingMode = nonActivePara2.style.writingMode = writingMode;
-        nonActivePara1.style.marginBottom = nonActivePara2.style.marginBottom = marginBottom;
+let isContactFormVisible = (div) => {
+    if (div.classList.contains("active") && div === contactDiv) {
         contactForm.style.display = "inline";
     } else {
-        
+        contactForm.style.display = "none";
     }
 }
 
 aboutDiv.onclick = () => {
     resetAllDivs();
-    addActiveClass(aboutDiv);
-    activeFormatting(aboutDiv, projectsDiv, projectsPara, contactPara);
+    addActiveClass(aboutDiv, aboutPara);
+    isContactFormVisible(aboutDiv);
+    changeLogo(aboutDiv)
 }
 
 projectsDiv.onclick = () => {
     resetAllDivs();
-    addActiveClass(projectsDiv);
-    activeFormatting(projectsDiv, aboutDiv, aboutPara, contactPara);
-    changeLogo(true);
+    addActiveClass(projectsDiv, projectsPara);
+    isContactFormVisible(projectsDiv);
+    changeLogo(projectsDiv);
 }
 
 contactDiv.onclick = () => {
     resetAllDivs();
-    addActiveClass(contactDiv);
-    activeFormatting(contactDiv, aboutDiv, aboutPara, projectsPara);
-    changeLogo(true);
+    addActiveClass(contactDiv, contactPara);
+    isContactFormVisible(contactDiv);
+    changeLogo(contactDiv);
 }
 
 let resetAllDivs = () => {
-    // aboutDiv.style.flexGrow = projectsDiv.style.flexGrow = contactDiv.style.flexGrow = 1;
-    changeLogo(false);
-    aboutPara.style.fontSize = projectsPara.style.fontSize = contactPara.style.fontSize = normalDivFontSize;
-    aboutPara.style.writingMode = projectsPara.style.writingMode = contactPara.style.writingMode = "horizontal-tb";
-    aboutPara.style.visibility = projectsPara.style.visibility = contactPara.style.visibility = "visible";
-    // aboutDiv.style.alignItems = "center";
-    contactForm.style.display = "none";
-    removeActiveClass(aboutDiv);
-    removeActiveClass(projectsDiv);
-    removeActiveClass(contactDiv);
+    changeLogo(aboutDiv);
+    removeActiveClass(aboutDiv, aboutPara);
+    removeActiveClass(projectsDiv, projectsPara);
+    removeActiveClass(contactDiv, contactPara);
 }
