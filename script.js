05a06dc8dae7a49a4a4daedc7f597013b2992703
Asia <Asia@miniprogramista.home>
2021-12-16 08:59:04 +0100

    Adding about text

--- script.js
+++ script.js
@@ -14,47 +14,48 @@ let contactForm = document.getElementById("contact-form");
 let logo = document.getElementById("logo");
 let submitBtn = document.getElementById("submit-btn");
 let projectsContainer = document.getElementById("project-container");
+let aboutText = document.getElementById("about-text");
 let logoSmallSize = "100px";
 let logoNormalSize = "300px";
 let bgColorOver = "#f8a23a";
 let bgColor = "#131b26";
 
+let addMouseEventListeners = function() {
+    for (let i = 0; i < navItems.length; i++) {
+        let div = navItems[i];
+        navItems[i].addEventListener("mouseover", function() {
+            changeColor(navItems[i], navPara[i], true);
+        });
+
+        navItems[i].addEventListener("mouseout", function() {
+            if (!div.classList.contains("active")) {
+                changeColor(navItems[i], navPara[i], false);
+            }
+        });
+    }
+}
+
+addMouseEventListeners();
+
 let changeColor = (div, para, toActive) => {
     if (toActive) {
         div.style.backgroundColor = bgColorOver;
         para.style.color = bgColor;
-        submitBtn.style.backgroundColor = bgColor;
-        submitBtn.style.color = bgColorOver;
     } else {
         div.style.backgroundColor = bgColor;
         para.style.color = bgColorOver;
-        submitBtn.style.backgroundColor = bgColorOver;
-        submitBtn.style.color = bgColor;
     }
 }
 
 let changeLogo = (div) => {
     if (div.classList.contains("active") && div === aboutDiv) {
         logo.style.width = logo.style.height = logoNormalSize;
+        logo.style.alignSelf = "center";
     } else {
         logo.style.width = logo.style.height = logoSmallSize;
+        logo.style.alignSelf = "flex-start";
     }
 }
-
-let bgColorChange = function(div) {
-    for (let i = 0; i < navItems.length; i++) {
-        navItems[i].addEventListener("mouseover", function() {
-            changeColor(navItems[i], navPara[i], true);
-        });
-
-        navItems[i].addEventListener("mouseout", function() {
-            if (div.classList.contains("nonactive")) {
-                changeColor(navItems[i], navPara[i], false);
-            }
-            });
-        }
-        
-    }
 
 let addActiveClass = (div, para) => {
     div.classList.add("active");
@@ -70,7 +71,7 @@ let removeActiveClass = (div, para) => {
     para.classList.add("nonactive-para");
 }
 
-let isContactFormVisible = (div) => {
+let visibilityOfContactForm = (div) => {
     if (div.classList.contains("active") && div === contactDiv) {
         contactForm.style.display = "inline";
     } else {
@@ -78,7 +79,7 @@ let isContactFormVisible = (div) => {
     }
 }
 
-let isProjectListVisible = (div) => {
+let visibilityOfProjectList = (div) => {
     if (div.classList.contains("active") && div === projectsDiv) {
         projectsContainer.style.display = "flex";
     } else {
@@ -86,31 +87,45 @@ let isProjectListVisible = (div) => {
     }
 }
 
+let visibilityOfAboutText = (div) => {
+    if (div.classList.contains("active") && div === aboutDiv) {
+        aboutText.style.display = "flex";
+    } else {
+        aboutText.style.display  = "none";
+    }
+}
+
 aboutDiv.onclick = () => {
     resetAllDivs();
     addActiveClass(aboutDiv, aboutPara);
-    isContactFormVisible(aboutDiv);
-    isProjectListVisible(aboutDiv);
+    visibilityOfContactForm(aboutDiv);
+    visibilityOfProjectList(aboutDiv);
+    visibilityOfAboutText(aboutDiv);
     changeLogo(aboutDiv)
-    bgColorChange(aboutDiv);
+    changeColor(projectsDiv, projectsPara, false);
+    changeColor(contactDiv, contactPara, false);
 }
 
 projectsDiv.onclick = () => {
     resetAllDivs();
     addActiveClass(projectsDiv, projectsPara);
-    isContactFormVisible(projectsDiv);
-    isProjectListVisible(projectsDiv);
+    visibilityOfContactForm(projectsDiv);
+    visibilityOfProjectList(projectsDiv);
+    visibilityOfAboutText(projectsDiv);
     changeLogo(projectsDiv);
-    bgColorChange(projectsDiv);
+    changeColor(aboutDiv, aboutPara, false);
+    changeColor(contactDiv, contactPara, false);
 }
 
 contactDiv.onclick = () => {
     resetAllDivs();
     addActiveClass(contactDiv, contactPara);
-    isContactFormVisible(contactDiv);
-    isProjectListVisible(contactDiv);
+    visibilityOfContactForm(contactDiv);
+    visibilityOfProjectList(contactDiv);
+    visibilityOfAboutText(contactDiv);
     changeLogo(contactDiv);
-    bgColorChange(contactDiv);
+    changeColor(aboutDiv, aboutPara, false);
+    changeColor(projectsDiv, projectsPara, false);
 }
 
 let resetAllDivs = () => {
